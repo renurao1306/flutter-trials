8f89f2d8081474b4b735cf1f30fe878442482b03
renurao1306 <renurao.1306@gmail.com>
2024-05-18 12:08:24 +0530

    hello world w gradient background color

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,13 @@ void main() {
     MaterialApp(
       home: Scaffold(
         body: Container(
-          color: Colors.amber,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.pink, Colors.white],
+            ),
+          ),
           child: const Center(
             child: Text('Hello world'),
           ),
