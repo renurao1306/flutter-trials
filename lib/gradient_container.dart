2fa12c6f35a702082dae474e9c598b3d1e88c42c
renurao1306 <renurao.1306@gmail.com>
2024-05-20 14:19:40 +0530

    added images and button

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,28 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
+// import 'package:first_app/styled_text.dart';
 
 var startAlignment = Alignment.topCenter;
 const endAlignment = Alignment.bottomCenter;
 
-
-class GradientContainer extends StatelessWidget{
+class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key, required this.colors});
 
   final List<Color> colors;
 
+  void rollDice() {}
+
   @override
   Widget build(BuildContext context) {
     return Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: startAlignment,
-              end: endAlignment,
-              colors: colors,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: startAlignment,
+          end: endAlignment,
+          colors: colors,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/dice-1.png',
+              width: 200,
             ),
-          ),
-          child: const Center(
-            child: StyledText('Hello Renu!'),
-          ),
-        );
+            const SizedBox(height: 20),
+            OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                    backgroundColor: Colors.greenAccent,
+                    foregroundColor: Colors.black,
+                    side: const BorderSide(width: 2)
+                    ),
+                onPressed: rollDice,
+                child: const Text(
+                  'Roll Dice',
+                  style: TextStyle(fontSize: 30),
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
